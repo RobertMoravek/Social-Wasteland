278e28297f269ccf3ff833c09f743fe9a4015529
Robert Morávek <robert.moravek@gmail.com>
2022-08-24 14:30:01 +0200

    loaded and sorted lots of data

--- client/src/components/otherUserProfile.js
+++ client/src/components/otherUserProfile.js
@@ -1,17 +1,16 @@
 import { useParams } from "react-router";
 import { useState, useEffect } from "react";
-import {Redirect} from  "react-router-dom";
+import { Redirect } from "react-router-dom";
 import FriendshipButton from "./friendshipButton";
 import { useHistory } from "react-router-dom";
-
 
 export default function ShowOtherUsers(userId) {
     const history = useHistory();
     const { id } = useParams();
     let [userInfo, setUserInfo] = useState({});
 
-    useEffect (() => {
-        if ( id == userId.userId) {
+    useEffect(() => {
+        if (id == userId.userId) {
             history.push("/");
         } else {
             fetch(`/loadotheruserinfo/${id}`)
@@ -19,12 +18,11 @@ export default function ShowOtherUsers(userId) {
                 .then((result) => {
                     if (!result.error) {
                         if (userId.userId != "") {
-                            if(result.rowCount > 0){
+                            if (result.rowCount > 0) {
                                 setUserInfo(result.rows[0]);
                             } else {
                                 history.replace("/users");
                             }
-
                         }
                         // console.log(this.state);
                     } else {
@@ -33,23 +31,37 @@ export default function ShowOtherUsers(userId) {
                         // location.reload();
                         // this.state.error = true;
                     }
-                });}
+                });
+        }
     }, [id, userId]);
 
-
+    function lowerCaseNames(first, last) {
+        let firstVar = first.toLowerCase();
+        let lastVar = last.toLowerCase();
+        return `${firstVar} ${lastVar}`;
+    }
 
     return (
         <>
-            <div className="user">
-                <img src={userInfo.profile_pic_url || "../defaultprofile.jpg"} alt="" className="profile-image"/>
-                <p>
-                    {userInfo.firstname} {userInfo.lastname}
-                </p>
-                <p>{userInfo.bio}</p>
-                <FriendshipButton/>
+            <div className="component profile ">
+                <h2 className="component-headline">not you</h2>
+                <div className="component-content profile">
+                    <img
+                        src={
+                            userInfo.profile_pic_url || "../defaultprofile.jpg"
+                        }
+                        alt=""
+                        className="profile-image"
+                    />
+                    <div className="user-info ">
+                        <h3>
+                            {userInfo.firstname} {userInfo.lastname}
+                        </h3>
+                        <p className="extra-top-margin">{userInfo.bio}</p>
+                        <FriendshipButton />
+                    </div>
+                </div>
             </div>
-            
         </>
     );
-
 }
